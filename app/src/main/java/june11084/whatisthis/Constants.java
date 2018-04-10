9abaae21ed8ed3717ce1f2e8cc5e1957a78fc8e5
Jun Li <june11084@gmail.com>
2018-04-10 09:42:07 -0700

    create project, Constant file

---
+++ app/src/main/java/june11084/whatisthis/Constants.java
@@ -0,0 +1,19 @@
+package june11084.whatisthis;
+
+/**
+ * Created by june1 on 4/10/2018.
+ */
+
+public class Constants {
+    public static final String YELP_TOKEN = BuildConfig.GOOGLE_CLOUD_API;
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v3/businesses/search?term=restaurants";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+    public static final String PREFERENCES_LOCATION_KEY = "location";
+    public static final String FIREBASE_CHILD_RESTAURANTS = "restaurants";
+    public static final String FIREBASE_QUERY_INDEX = "index";
+    public static final String EXTRA_KEY_POSITION = "position";
+    public static final String EXTRA_KEY_RESTAURANTS = "restaurants";
+    public static final String KEY_SOURCE = "source";
+    public static final String SOURCE_SAVED = "saved";
+    public static final String SOURCE_FIND = "find";
+}
---
+++ app/src/main/java/june11084/whatisthis/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package june11084.whatisthis.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import june11084.whatisthis.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
